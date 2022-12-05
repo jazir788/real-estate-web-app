ee80cc370810019ecb6979142edd7487df8c8319
jazir788 <jazirsx5@gmail.com>
2022-12-05 14:26:46 +0000

    Complete the sign in functionality

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
 import {AiFillEye, AiFillEyeInvisible} from "react-icons/ai"
-import { Link } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 import OAuth from '../components/OAuth';
+import { signInWithEmailAndPassword, getAuth } from 'firebase/auth';
+import { toast } from 'react-toastify';
 
 const SignIn = () => {
+    const navigate = useNavigate();
     const[showPassword, setShowPassword] = useState(false);
     const[formData, setFormData] = useState({
         email:"",
@@ -17,6 +20,21 @@ const SignIn = () => {
  
         }))
     }
+    async function onSubmit(e){
+        e.preventDefault()
+        try {
+            const auth = getAuth()
+            const userCredentials = await signInWithEmailAndPassword(auth, email, password)
+            if(userCredentials.user){
+                navigate("/")
+            }
+            
+        } catch (error) {
+            toast.error("Bad User Credentials")
+            
+        }
+
+    }
   return (
     <section>
         <h1 className='text-3xl text-center mt-6 font-bold'>
@@ -27,7 +45,7 @@ const SignIn = () => {
             <img className= "w-full rounded 2xl" src ="https://images.unsplash.com/photo-1609770231080-e321deccc34c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80" alt="key" />            
         </div>
          <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-            <form>
+            <form onSubmit={onSubmit}>
                 <input className= 'mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out' type ="email" id="email" value={email} onChange={onChange} placeholder= "Email Address" />
             <div className='relative mb-6' >
                 <input className= 'w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out' type ={showPassword ? "text" : "password"} id="password" value={password} onChange={onChange} placeholder= "Password" />
